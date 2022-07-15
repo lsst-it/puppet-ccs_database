b2443d55eceffc23df1969dbe3fec998152ac058
Joshua Hoblitt <josh@hoblitt.com>
2022-07-15 11:06:09 -0700

    add OS context to spec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,23 +3,25 @@
 require 'spec_helper'
 
 describe 'ccs_database' do
-  on_supported_os.each do |_os, facts|
-    let(:facts) do
-      facts
-    end
-
-    describe 'with database & password parameters' do
-      let(:params) do
-        {
-          database: 'bar',
-          password: 'foo', # required to prevent lookup of 'ccs_dbarchive'
-        }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
       end
 
-      it { is_expected.to compile.with_all_deps }
-      it { is_expected.to contain_class('mysql::server') }
-      it { is_expected.to have_mysql__db_resource_count(1) }
-      it { is_expected.to contain_mysql__db('bar') }
+      describe 'with database & password parameters' do
+        let(:params) do
+          {
+            database: 'bar',
+            password: 'foo', # required to prevent lookup of 'ccs_dbarchive'
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('mysql::server') }
+        it { is_expected.to have_mysql__db_resource_count(1) }
+        it { is_expected.to contain_mysql__db('bar') }
+      end
     end
   end
 end
